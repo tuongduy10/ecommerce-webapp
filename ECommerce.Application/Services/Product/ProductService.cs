cf9a404765cb8beed3ddd8180127216f50b050ab
tuongduy10 <vtngduy101120@gmail.com>
2022-04-24 18:14:30 +0700

    display product by subcategory

--- ECommerce.Application/Services/Product/ProductService.cs
+++ ECommerce.Application/Services/Product/ProductService.cs
@@ -18,6 +18,63 @@ namespace ECommerce.Application.Services.Product
         {
             _DbContext = DbContext;
         }
+        public async Task<List<ProductInBrandModel>> getProductsInBrand(int BrandId, int pageindex, int pagesize)
+        {
+            var query = from product in _DbContext.Products
+                        join brand in _DbContext.Brands on product.BrandId equals brand.BrandId
+                        join shop in _DbContext.Shops on product.ShopId equals shop.ShopId
+                        where brand.BrandId == BrandId
+                        orderby product.SubCategoryId
+                        select new
+                        {
+                            id = product.ProductId,
+                            name = product.ProductName,
+                            discount = product.DiscountPercent,
+                            status = product.Status,
+                            highlights = product.Highlights,
+                            newPro = product.New,
+                            importDate = product.ProductImportDate,
+                            subcategory = product.SubCategoryId,
+
+                            brandName = brand.BrandName,
+                            shopName = shop.ShopName,
+                        };
+
+            var list = query.AsQueryable().Select(i => new ProductInBrandModel()
+            {
+                ProductId = i.id,
+                ProductName = i.name,
+                DiscountPercent = i.discount,
+                Status = i.status,
+                Highlights = i.highlights,
+                New = i.newPro,
+                ProductImportDate = i.importDate,
+                SubCategoryId = i.subcategory,
+
+                Type = (
+                    from product in _DbContext.Products
+                    from price in _DbContext.ProductPrices
+                    from type in _DbContext.ProductTypes
+                    where product.ProductId == i.id &&
+                          price.ProductTypeId == type.ProductTypeId && price.ProductId == product.ProductId
+                    select new { id = type.ProductTypeId, name = type.ProductTypeName }
+                ).Select(t => new Dtos.Type()
+                {
+                    ProductTypeId = t.id,
+                    ProductTypeName = t.name
+                }).ToList(),
+
+                Price = _DbContext.ProductPrices.Where(price => price.ProductId == i.id).ToList(),
+                ProductImages = _DbContext.ProductImages.Where(img => img.ProductId == i.id).Select(i => i.ProductImagePath).FirstOrDefault(),
+
+                Brand = i.brandName,
+                Shop = i.shopName,
+            });
+
+            var result = await PaginatedList<ProductInBrandModel>.CreateAsync(list, pageindex, pagesize);
+
+            return result;
+        }
         public async Task<PageResult<ProductInBrandModel>> getProductPaginated(int BrandId, int pageindex, int pagesize)
         {
             // Query get products with brand, shop, subcategoryid
@@ -86,12 +143,13 @@ namespace ECommerce.Application.Services.Product
 
             return result;
         }
-        public async Task<List<ProductInBrandModel>> getProductsInBrand(int BrandId, int pageindex, int pagesize)
+        public async Task<PageResult<ProductInBrandModel>> getProductBySubCategoryPaginated(int BrandId, int SubCategoryId, int pageindex, int pagesize)
         {
+            // Query get products with brand, shop, subcategoryid
             var query = from product in _DbContext.Products
                         join brand in _DbContext.Brands on product.BrandId equals brand.BrandId
                         join shop in _DbContext.Shops on product.ShopId equals shop.ShopId
-                        where brand.BrandId == BrandId
+                        where brand.BrandId == BrandId && product.SubCategoryId == SubCategoryId
                         orderby product.SubCategoryId
                         select new
                         {
@@ -107,7 +165,7 @@ namespace ECommerce.Application.Services.Product
                             brandName = brand.BrandName,
                             shopName = shop.ShopName,
                         };
-
+            // Excute query to list
             var list = query.AsQueryable().Select(i => new ProductInBrandModel()
             {
                 ProductId = i.id,
@@ -139,14 +197,19 @@ namespace ECommerce.Application.Services.Product
                 Shop = i.shopName,
             });
 
-            var result = await PaginatedList<ProductInBrandModel>.CreateAsync(list, pageindex, pagesize);
+            var record = await list.CountAsync();
+            var data = await PaginatedList<ProductInBrandModel>.CreateAsync(list, pageindex, pagesize);
+
+            var result = new PageResult<ProductInBrandModel>()
+            {
+                Items = data,
+                CurrentRecord = (pageindex * pagesize) <= record ? (pageindex * pagesize) : record,
+                TotalRecord = record,
+                CurrentPage = pageindex,
+                TotalPage = (int)Math.Ceiling(record / (double)pagesize)
+            };
 
             return result;
-        }
-    
-        public async Task<PageResult<ProductInBrandModel>> getProductBySubCategoryPaginated(int BrandId, int SubCategoryId, int pageindex, int pagesize)
-        {
-            return null;
         }
     }
 }
--- ECommerce.WebApp/APIs/ProductAPI.cs
+++ ECommerce.WebApp/APIs/ProductAPI.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerce.Application.Services.Brand;
+using ECommerce.Application.Services.FilterProduct;
 using ECommerce.Application.Services.Product;
 using ECommerce.Application.Services.SubCategory;
 using ECommerce.WebApp.Models.Products;
@@ -18,11 +19,13 @@ namespace ECommerce.WebApp.APIs
         private readonly IProductService _productService;
         private readonly ISubCategoryService _subCategoryService;
         private readonly IBrandService _brandService;
-        public ProductAPI(IProductService productService, ISubCategoryService subCategoryService, IBrandService brandService)
+        private readonly IFilterProductService _filterService;
+        public ProductAPI(IProductService productService, ISubCategoryService subCategoryService, IBrandService brandService, IFilterProductService filterService)
         {
             _productService = productService;
             _subCategoryService = subCategoryService;
             _brandService = brandService;
+            _filterService = filterService;
         }
 
         [HttpGet("getProductInBrand")]
@@ -32,12 +35,33 @@ namespace ECommerce.WebApp.APIs
             var listProduct = await _productService.getProductPaginated(BrandId, pageIndex, itemsInPage);
             var listSubCategory = await _subCategoryService.getSubCategoryInBrand(BrandId);
             var brand = await _brandService.getBrandById(BrandId);
+            var filter = await _filterService.listFilterModel(BrandId);
 
             var model = new ProductInBrandViewModel()
             {
                 listProduct = listProduct,
                 listSubCategory = listSubCategory,
-                brand = brand
+                brand = brand,
+                listFilterModel = filter,
+            };
+
+            return Ok(new { status = "success", data = model });
+        }
+        [HttpGet("getProductBySubCategory")]
+        public async Task<IActionResult> getProductBySubCategory([FromQuery] int BrandId, int SubCategoryId, int pageIndex = 1)
+        {
+            int itemsInPage = 30;
+            var listProduct = await _productService.getProductBySubCategoryPaginated(BrandId, SubCategoryId, pageIndex, itemsInPage);
+            var listSubCategory = await _subCategoryService.getSubCategoryInBrand(BrandId);
+            var brand = await _brandService.getBrandById(BrandId);
+            var filter = await _filterService.listFilterModel(BrandId);
+
+            var model = new ProductInBrandViewModel()
+            {
+                listProduct = listProduct,
+                listSubCategory = listSubCategory,
+                brand = brand,
+                listFilterModel = filter
             };
 
             return Ok(new { status = "success", data = model });
