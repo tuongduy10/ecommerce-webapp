169adb3444e6cbd3af5cee74da4056c7ed0a0a02
MIDORI\Thanh Phat <trinhphat144@gmail.com>
2022-10-29 23:37:17 +0700

    attribute repo

---
+++ ECommerce.Application/Repositories/Attribute/AttributeRepository.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Repositories.Attribute
+{
+    public class AttributeRepository : RepositoryBase<Data.Models.Attribute>, IAttributeRepository
+    {
+        public AttributeRepository(ECommerceContext DbContext) : base(DbContext)
+        {
+
+        }
+    }
+}
