3c9deb6b4066f22d6a6125374c11fa72e4267000
MIDORI\Thanh Phat <trinhphat144@gmail.com>
2022-11-01 21:43:33 +0700

    Brand complete

---
+++ ECommerce.Application/Repositories/Brand/BrandRepository.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Application.Repositories.Category;
+using ECommerce.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Repositories.Brand
+{
+    public class BrandRepository : RepositoryBase<Data.Models.Brand>, IBrandRepository
+    {
+        public BrandRepository(ECommerceContext DbContext) : base(DbContext)
+        {
+
+        }
+    }
+}
---
+++ ECommerce.Application/Repositories/Brand/IBrandRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Repositories.Brand
+{
+    public interface IBrandRepository : IRepositoryBase<Data.Models.Brand>
+    {
+    }
+}
