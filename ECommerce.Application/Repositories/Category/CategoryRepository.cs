6a053074356839cf678f4116aabc37295a967a53
MIDORI\Thanh Phat <trinhphat144@gmail.com>
2022-11-01 21:35:53 +0700

    Category complete

---
+++ ECommerce.Application/Repositories/Category/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using ECommerce.Application.Repositories.Bank;
+using ECommerce.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Repositories.Category
+{
+    public class CategoryRepository : RepositoryBase<Data.Models.Category>, ICategoryRepository
+    {
+        public CategoryRepository(ECommerceContext DbContext) : base(DbContext)
+        {
+
+        }
+    }
+}
