32e9c58710a158ac1441817d2b72a07742a63cd4
MIDORI\Thanh Phat <trinhphat144@gmail.com>
2022-11-06 23:54:52 +0700

    Create interface, class (Product)

---
+++ ECommerce.Application/Repositories/Product/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Repositories.Product
+{
+    public interface IProductRepository : IRepositoryBase<Data.Models.Product>
+    {
+    }
+}
---
+++ ECommerce.Application/Repositories/Product/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Repositories.Product
+{
+    public class ProductRepository : RepositoryBase<Data.Models.Product>, IProductRepository
+    {
+        public ProductRepository(ECommerceContext DbContext) : base(DbContext)
+        {
+
+        }
+    }
+}
