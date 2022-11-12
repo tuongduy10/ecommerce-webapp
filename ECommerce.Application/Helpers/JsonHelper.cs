66ea80693a77580f9bb91c76222e5a981cdaa7b5
tuongduy10 <vtngduy101120@gmail.com>
2022-11-12 14:52:22 +0700

    Add json converter (helper)

---
+++ ECommerce.Application/Helpers/JsonHelper.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Application.Helpers
+{
+    public class JsonHelper<T> where T : class
+    {
+        public string Stringify(T obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+        public T Parse(string str)
+        {
+            return JsonConvert.DeserializeObject<T>(str);
+        }
+    }
+}
