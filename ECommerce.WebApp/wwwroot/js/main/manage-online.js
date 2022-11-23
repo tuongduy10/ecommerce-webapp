ab95e5703eb27366699eef6dab902cfd9f0f7574
tuongduy10 <vtngduy101120@gmail.com>
2022-11-23 23:03:56 +0700

    Update js files storage location

---
+++ ECommerce.WebApp/wwwroot/js/main/manage-online.js
@@ -0,0 +1,25 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/onlineHub").build();
+connection.start().then(function () {
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+connection.on("ReceiveOnlineUsers", function (res) {
+    if (res && res.data) {
+        console.log(res.data);
+        $(`.user-status`).html(htmlOffline);
+        res.data.forEach(user => {
+            document.getElementById(`user-status-${user.userId}`).innerHTML = htmlOnline;
+        });
+    }
+});
+
+// html
+const htmlOnline = `
+    <div class="mr-2" style="height: 18px; width: 18px; background: #31A24C; border-radius: 50%"></div>
+    <label class="mb-0" style="color: #31A24C">Đang hoạt động</label>
+`;
+const htmlOffline = `
+    <div class="mr-2" style="height: 18px; width: 18px; background: #a1a3a6; border-radius: 50%"></div>
+    <label class="mb-0" style="color: #a1a3a6">Không hoạt động</label>
+`;
---
+++ ECommerce.WebApp/wwwroot/js/main/online.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/onlineHub").build();
+// Connect to server
+connection.start().then(function () {
+    // Call function from server
+    connection.invoke("SendOnlineUsers").catch(function (err) {
+        return console.error(err.toString());
+    });
+}).catch(function (err) {
+    return console.error(err.toString());
+});
