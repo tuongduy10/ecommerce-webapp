55e1bf5f4a16d8987fc58a89805c36fd6166181d
tuongduy10 <vtngduy101120@gmail.com>
2023-01-09 23:00:14 +0700

    Tinymce init config

--- ECommerce.Application/Constants/ConfigConstant.cs
+++ ECommerce.Application/Constants/ConfigConstant.cs
@@ -4,8 +4,12 @@ using System.Text;
 
 namespace ECommerce.Application.Constants
 {
-    public  class ConfigConstant
+    public class ConfigConstant
     {
         public const string DATE_FORMAT = "HH:mm:ss, dd/MM/yyyy";
+
+        // tinymce init config
+        public const string TINY_PLUGINS = "image autolink lists media table";
+        public const string TINY_TOOLBAR = "addcomment showcomments code image pageembed permanentpen table tableofcontents";
     }
 }
