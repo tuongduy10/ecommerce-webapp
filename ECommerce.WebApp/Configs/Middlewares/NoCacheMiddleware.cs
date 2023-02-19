b8edabc348ad3cb8fdc848b2d091d2a4a2d869b8
tuongduy10 <vtngduy101120@gmail.com>
2023-02-19 18:55:09 +0700

    [ECM-Config] Add no saving cache

---
+++ ECommerce.WebApp/Configs/Middlewares/NoCacheMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ECommerce.WebApp.Configs.Middlewares
+{
+    public class NoCacheMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public NoCacheMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+            context.Response.Headers["Pragma"] = "no-cache";
+            context.Response.Headers["Expires"] = "0";
+
+            await _next(context);
+        }
+    }
+}
