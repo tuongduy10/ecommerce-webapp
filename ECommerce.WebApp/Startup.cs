bf2ae24396e53d7e4a786da20fbfd648f3f39259
tuongduy10 <vtngduy101120@gmail.com>
2023-07-08 16:02:20 +0700

    ignore file build

--- ECommerce.WebApp/Startup.cs
+++ ECommerce.WebApp/Startup.cs
@@ -138,6 +138,12 @@ namespace ECommerce.WebApp
             //services.Configure<SecurityStampValidatorOptions>(option => option.ValidationInterval = TimeSpan.FromSeconds(10));
             //services.AddHttpContextAccessor();
             //services.AddScoped<IUserService>();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "client/build";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
