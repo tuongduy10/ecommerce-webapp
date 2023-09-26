42b29c6ad3f92fa3c07348cb9a0ca97e8e83eb04
tuongduy10 <duy.vo@bstarsolutions.com>
2023-09-26 17:43:59 +0700

    Product service

---
+++ ECommerce.Application/Services/Product/Dtos/ProductGetRequest.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.Services.Product.Dtos
+{
+    public class ProductGetRequest : PageRequest
+    {
+        public int? id { get; set; }
+        public int brandId { get; set; }
+        public int subCategoryId { get; set; }
+        public string orderBy { get; set; } // newest | discount
+        public List<int> optionValueIds { get; set; }
+        public string getBy { get; set; }
+
+        public ProductGetRequest()
+        {
+            id = -1;
+            brandId = -1;
+            subCategoryId = -1;
+        }
+    }
+}
--- ECommerce.WebApp/Controllers/ProductController.cs
+++ ECommerce.WebApp/Controllers/ProductController.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ECommerce.WebApp.Controllers
