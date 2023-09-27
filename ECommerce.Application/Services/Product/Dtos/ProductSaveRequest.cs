56ad24ad71953f4c2810eff2b3085f5ca5acada3
tuongduy10 <vtngduy101120@gmail.com>
2023-09-27 18:53:53 +0700

    Null validation and remove IFormFile

---
+++ ECommerce.Application/Services/Product/Dtos/ProductSaveRequest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.Services.Product.Dtos
+{
+    public class ProductSaveRequest : ProductModel
+    {
+        private List<IFormFile>? systemImage { get; set; }
+        private List<IFormFile>? userImage { get; set; }
+        public ProductSaveRequest() : base()
+        {
+
+        }
+    }
+}
