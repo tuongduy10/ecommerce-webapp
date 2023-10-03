e26f25df79fee9e423504cdef72a437f57879d09
tuongduy10 <duy.vo@bstarsolutions.com>
2023-10-03 17:27:41 +0700

    Add base api to integrate and dummy data

--- ECommerce.WebApp/client/src/_pages/product-list/_components/prol-category.tsx
+++ ECommerce.WebApp/client/src/_pages/product-list/_components/prol-category.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { IOption, IOptionValue, ISubCategory } from "src/_cores/_interfaces/inventory.interface";
+import { useProductStore } from "src/_cores/_store/root-store";
 import MuiIcon from "src/_shares/_components/mui-icon/mui-icon.component";
 
 const ProlCategory = () => {
+  const productStore = useProductStore();
   const [isExpanded, setIsExpanded] = useState(false);
   const [isFeatherIconChanged, setFeatherIconChanged] = useState(false);
 
@@ -12,137 +15,58 @@ const ProlCategory = () => {
   return (
     <div className="products__menu">
       <div className="product__filter">
-        <div className="filter__block">
-          <h3 className="filter__title" onClick={toggleExpand}>
-            Máy pha cà phê
-            <MuiIcon
-              name="CHEVRON_RIGHT"
-              className={`feather feather-chevron-right ${
-                isFeatherIconChanged ? "svg-right" : ""
-              }`}
-            />
-          </h3>
-
-          <ul
-            className="filter__list filter--scrollbar"
-            style={{
-              maxHeight: isExpanded
-                ? "calc(185px + 125 * ((100vw - 375px) / 1545))"
-                : "0px",
-            }}
-          >
-            <li className="filter__item">
-              <p className="mb-0">
-                Màu sắc
-              </p>
-              <ul className="filter__sub-list">
-                <li className="filter__sub-item">
-                  <div className="checkbox flex items-center">
-                    <input
-                      id="sub00"
-                      className="checkboxFilter"
-                      type="checkbox"
-                    />
-                    <label
-                      htmlFor="sub00"
-                      className="flex items-center mb-0"
-                    >
-                      <div className="sub-item-cate">Tím</div>
-                      <span className="sub-item-cate">(10)</span>
-                    </label>
-                  </div>
-                </li>
-                <li className="filter__sub-item">
-                  <div className="checkbox flex items-center">
-                    <input
-                      id="sub01"
-                      className="checkboxFilter"
-                      type="checkbox"
-                    />
-                    <label
-                      htmlFor="sub01"
-                      className="flex items-center mb-0"
-                    >
-                      <div className="sub-item-cate">Tím</div>
-                      <span className="sub-item-cate">(10)</span>
-                    </label>
-                  </div>
-                </li>
-                <li className="filter__sub-item">
-                  <div className="checkbox flex items-center">
-                    <input
-                      id="sub02"
-                      className="checkboxFilter"
-                      type="checkbox"
-                    />
-                    <label
-                      htmlFor="sub02"
-                      className="flex items-center mb-0"
-                    >
-                      <div className="sub-item-cate">Tím</div>
-                      <span className="sub-item-cate">(10)</span>
-                    </label>
-                  </div>
-                </li>
+        {productStore.subCategories.length > 0 && productStore.subCategories.map((sub: ISubCategory) => (
+          <div key={`sub-${sub.id}`} className="filter__block">
+            <h3 className="filter__title" onClick={toggleExpand}>
+              {sub.name}
+              <MuiIcon
+                name="CHEVRON_RIGHT"
+                className={`feather feather-chevron-right ${isFeatherIconChanged ? "svg-right" : ""
+                  }`}
+              />
+            </h3>
+            {sub.optionList && sub.optionList.length > 0 && (
+              <ul
+                className="filter__list filter--scrollbar"
+                style={{
+                  maxHeight: isExpanded
+                    ? "calc(185px + 125 * ((100vw - 375px) / 1545))"
+                    : "0px",
+                }}
+              >
+                {sub.optionList.map((option: IOption) => (
+                  <li key={`option-${option.id}`} className="filter__item">
+                    <p className="mb-0">
+                      {option.name}
+                    </p>
+                    {option.values && option.values.length > 0 && (
+                      <ul className="filter__sub-list">
+                        {option.values.map((_value: IOptionValue) => (
+                          <li key={`option-value-${_value.id}`} className="filter__sub-item">
+                            <div className="checkbox flex items-center">
+                              <input
+                                id="sub00"
+                                className="checkboxFilter"
+                                type="checkbox"
+                              />
+                              <label
+                                htmlFor="sub00"
+                                className="flex items-center mb-0"
+                              >
+                                <div className="sub-item-cate">{_value.name}</div>
+                                <span className="sub-item-cate">({_value.totalRecord})</span>
+                              </label>
+                            </div>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                ))}
               </ul>
-            </li>
-            <li className="filter__item">
-              <p className="mb-0">
-                Kích thước
-              </p>
-              <ul className="filter__sub-list">
-                <li className="filter__sub-item">
-                  <div className="checkbox flex items-center">
-                    <input
-                      id="sub1"
-                      className="checkboxFilter"
-                      type="checkbox"
-                    />
-                    <label
-                      htmlFor="sub1"
-                      className="flex items-center mb-0"
-                    >
-                      <div className="sub-item-cate">To</div>
-                      <span className="sub-item-cate">(10)</span>
-                    </label>
-                  </div>
-                </li>
-                <li className="filter__sub-item">
-                  <div className="checkbox flex items-center">
-                    <input
-                      id="sub2"
-                      className="checkboxFilter"
-                      type="checkbox"
-                    />
-                    <label
-                      htmlFor="sub2"
-                      className="flex items-center mb-0"
-                    >
-                      <div className="sub-item-cate">Vừa</div>
-                      <span className="sub-item-cate">(10)</span>
-                    </label>
-                  </div>
-                </li>
-                <li className="filter__sub-item">
-                  <div className="checkbox flex items-center">
-                    <input
-                      id="sub3"
-                      className="checkboxFilter"
-                      type="checkbox"
-                    />
-                    <label
-                      htmlFor="sub3"
-                      className="flex items-center mb-0"
-                    >
-                      <div className="sub-item-cate">Nhỏ</div>
-                      <span className="sub-item-cate">(10)</span>
-                    </label>
-                  </div>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
--- ECommerce.WebApp/client/src/_pages/product-list/product-list.page.tsx
+++ ECommerce.WebApp/client/src/_pages/product-list/product-list.page.tsx
@@ -5,25 +5,111 @@ import { useEffect } from "react";
 import ProductService from "src/_cores/_services/product.service";
 import { useProductStore } from "src/_cores/_store/root-store";
 import { useDispatch } from "react-redux";
-import { setParam, setProductList } from "src/_cores/_reducers/product.reducer";
+import { setParam, setProductList, setSubCategories } from "src/_cores/_reducers/product.reducer";
+import InventoryService from "src/_cores/_services/inventory.service";
+
+const dummData = [
+  {
+    id: 1,
+    name: "sub category 1",
+    categoryId: 1,
+    optionList: [
+      {
+        id: 1,
+        name: "option 1",
+        values: [
+          {
+            id: 11,
+            name: "option value 11",
+            totalRecord: 100 // total product record
+          },
+          {
+            id: 12,
+            name: "option value 12",
+            totalRecord: 100 // total product record
+          }
+        ]
+      },
+      {
+        id: 1,
+        name: "option 1",
+        values: [
+          {
+            id: 11,
+            name: "option value 11",
+            totalRecord: 100 // total product record
+          },
+          {
+            id: 12,
+            name: "option value 12",
+            totalRecord: 100 // total product record
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "sub category 2",
+    categoryId: 2,
+    optionList: [
+      {
+        id: 2,
+        name: "option",
+        values: [
+          {
+            id: 21,
+            name: "option value 21",
+            totalRecord: 100 // total product record
+          },
+          {
+            id: 22,
+            name: "option value 22",
+            totalRecord: 100 // total product record
+          },
+        ]
+      },
+      {
+        id: 1,
+        name: "option 1",
+        values: [
+          {
+            id: 11,
+            name: "option value 11",
+            totalRecord: 100 // total product record
+          },
+          {
+            id: 12,
+            name: "option value 12",
+            totalRecord: 100 // total product record
+          }
+        ]
+      }
+    ]
+  }
+];
 
 const ProductListPage = () => {
+  const searchParams = new URLSearchParams(window.location.search);
+  const _pageIndex = Number(searchParams.get('pageIndex'));
+  const _brandId = Number(searchParams.get('brandId'));
+  const _orderBy = searchParams.get('orderBy');
+
   const productStore = useProductStore();
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const _pageIndex = searchParams.get('pageIndex');
-    const _brandId = searchParams.get('brandId');
-    const _orderBy = searchParams.get('orderBy');
-
     const params = {
-      pageIndex: Number(_pageIndex),
-      brandId: Number(_brandId),
+      pageIndex: _pageIndex,
+      brandId: _brandId,
       orderBy: _orderBy ?? '',
     }
     getData(params);
-  }, [productStore.param.pageIndex, productStore.param.orderBy]);
+  }, [_pageIndex, _orderBy]);
+  
+  useEffect(() => {
+    getSubCategories(_brandId);
+  }, [_brandId])
 
   const getData = (params: any) => {
     ProductService.getProductList(params).then((res: any) => {
@@ -39,7 +125,18 @@ const ProductListPage = () => {
         dispatch(setParam(param));
         dispatch(setProductList(_data.items));
       }
-    })
+    });
+  }
+
+  const getSubCategories = (brandId: number) => {
+    InventoryService.getSubCategories(brandId).then((res: any) => {
+      if (res.data) {
+        
+      }
+    }).catch(error => {
+      console.log(error);
+      dispatch(setSubCategories(dummData))
+    });
   }
 
   return (
