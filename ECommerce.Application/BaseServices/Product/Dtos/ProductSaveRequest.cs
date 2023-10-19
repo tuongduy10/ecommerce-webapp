aaf361543c6a756f74476f4154ba5e5cdbd3baf0
tuongduy10 <duy.vo@bstarsolutions.com>
2023-10-19 09:27:39 +0700

    Update request and save api

--- ECommerce.Application/BaseServices/Product/Dtos/ProductSaveRequest.cs
+++ ECommerce.Application/BaseServices/Product/Dtos/ProductSaveRequest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerce.Application.Services.Inventory.Dtos;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,9 @@ namespace ECommerce.Application.Services.Product.Dtos
 {
     public class ProductSaveRequest : ProductModel
     {
+
+        public List<int>? currentOptions { get; set; }
+        public List<OptionModel>? newOptions { get; set; }
         private List<IFormFile>? systemImage { get; set; }
         private List<IFormFile>? userImage { get; set; }
         public ProductSaveRequest() : base()
