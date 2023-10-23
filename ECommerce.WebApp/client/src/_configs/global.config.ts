bf3c81aa29ef43e6e3b48e39c9b9cdbbabb2cab0
tuongduy10 <duy.vo@bstarsolutions.com>
2023-10-23 17:32:23 +0700

    Select and autocomplete

--- ECommerce.WebApp/client/src/_configs/global.config.ts
+++ ECommerce.WebApp/client/src/_configs/global.config.ts
@@ -2,7 +2,8 @@ export const GlobalConfig = {
     MAX_PAGE_SIZE: 50,
     MIN_PAGE_SIZE: 5,
     ITEMS_PER_PAGE: 10,
-    TINY_PLUGINS: 'image autolink lists media table',
+    TINY_KEY: '4ctr9pi6o8mlflu8si6gdszgl62tn4zl73knhf3tkqalszsx',
+    TINY_PLUGINS: ["image", "autolink", "lists", "media", "table"],
     TINY_TOOLBAR: 'addcomment showcomments code image pageembed permanentpen table tableofcontents',
     // etc
 }
